d069f581e0ebc12fbdf56bb151628a45bfc4b62c
Oyebolaji Akinyemi <akinyemo@flip2.engr.oregonstate.edu>
2021-03-07 21:22:07 -0800

    Added Third Test to see if multiples of 400 are caught

--- TestLeapYear.py
+++ TestLeapYear.py
@@ -9,6 +9,9 @@ class TestLeapYear(unittest.TestCase):
 
     def test_non_mult_100(self):
         self.assertEqual(LeapYear.calc_Leap(2100),"No")
+    
+    def test_non_mult_400(self):
+        self.assertEqual(LeapYear.calc_Leap(2400),"No")
 
 if __name__ == "__main__":
     unittest.main()
