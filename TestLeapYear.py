2df1737e199f44b89883a016494572a1a832a65c
Oyebolaji Akinyemi <akinyemo@flip2.engr.oregonstate.edu>
2021-03-07 21:18:55 -0800

    Added Implementation for Second Test

--- TestLeapYear.py
+++ TestLeapYear.py
@@ -7,5 +7,8 @@ class TestLeapYear(unittest.TestCase):
     def test_non_leap(self):
         self.assertEqual(LeapYear.calc_Leap(2005),"No")
 
+    def test_non_mult_100(self):
+        self.assertEqual(LeapYear.calc_Leap(2100),"No")
+
 if __name__ == "__main__":
     unittest.main()
